c77bc590ac3ad0714239561db2fe6d07fb632297
VincenzoZ24 <zitov287@gmail.com>
2022-12-13 14:46:18 +0100

    censura parola e stampa lunghezza p censurato

--- index.php
+++ index.php
@@ -9,9 +9,18 @@
 <body>
     <?php
     $paragrafo = 'domani ho lezione per il corso di Boolean, faremo la seconda lezione di PHP, boolean è un corso di programmazione';
-    
+    $parola_censurata = $_GET['censura'];
+    $paragrafo_modificato = str_ireplace($parola_censurata, "***", $paragrafo);
     ?>
+
     <h1><?php echo $paragrafo?></h1>
     <h3>lunghezza paragrafo: <?php echo strlen($paragrafo)?></h3> 
+    <form action="" method="get">
+        <label for="censura">parola da censurare</label>
+        <input type="text" id="censura" name="censura">
+        <button>CENSURA</button>
+    </form>
+    <h3>paragrafo cesurato: <?php  echo $paragrafo_modificato ?></h3>
+    <h3>lunghezza paragrafo censurato <?php echo strlen($paragrafo_modificato)?></h3>
 </body>
 </html>
